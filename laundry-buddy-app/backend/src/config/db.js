ed473c1b0c44c3caaf4554973ea3b067877d94d3
xenthm <56202777+xenthm@users.noreply.github.com>
2024-05-31 04:45:59 +0800

    added runtime error listeners
    -

--- laundry-buddy-app/backend/src/config/db.js
+++ laundry-buddy-app/backend/src/config/db.js
@@ -5,8 +5,20 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected');
+
+    // Listen for runtime errors
+    mongoose.connection.on('error', err => {
+      console.error('Runtime connection error: ', err.message);
+    })
+    .on('disconnected', () => {
+      console.warn('Mongoose connection disconnected');
+      console.log('Attempting to reconnect to MongoDB...'); // Mongoose will attempt to reconnect
+    })
+    .on('reconnected', () => {
+      console.log('Mongoose reconnected to MongoDB');
+    });
   } catch (err) {
-    console.error(err.message);
+    console.error('Initial connection error: ', err.message);
     process.exit(1); // Exit process with failure
   }
 };
