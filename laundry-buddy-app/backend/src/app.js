6fadd768482482e9f02c8d856cf62448a7adef5e
xenthm <56202777+xenthm@users.noreply.github.com>
2024-05-31 19:31:50 +0800

    cleanup
    -

--- laundry-buddy-app/backend/src/app.js
+++ laundry-buddy-app/backend/src/app.js
@@ -5,19 +5,16 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-// Connect to database
 connectDB();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Middleware to handle errors
-app.use
-
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 
+// Global error handling
 app.use((err, req, res, next) => {
     console.error(err.message);
     res.status(500).send('Server error');
