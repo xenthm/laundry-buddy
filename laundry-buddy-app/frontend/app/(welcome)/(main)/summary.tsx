b9401aadfcb17199d68002a6d67fc6b370d263ff
sarahchow03 <76396931+sarahchow03@users.noreply.github.com>
2024-07-08 01:42:39 +0800

    Statistics page layout
    Created layout of statistics page based on Miro mock-up

--- laundry-buddy-app/frontend/app/(welcome)/(main)/summary.tsx
+++ laundry-buddy-app/frontend/app/(welcome)/(main)/summary.tsx
@@ -1,34 +1,76 @@
-// TODO: dashboard
-import { Tabs } from "expo-router";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { NavigationContainer } from "@react-navigation/native";
-import { Link, router } from "expo-router";
 import React, { useState } from "react";
+import { BarChart } from "react-native-gifted-charts";
 import {
-  Alert,
-  Image,
-  Pressable,
+  ImageBackground,
   SafeAreaView,
   StyleSheet,
-  Text,
-  TextInput,
   View,
-  ImageBackground,
+  Text,
+  Alert,
+  ScrollView,
 } from "react-native";
-import { BorderlessButton } from "react-native-gesture-handler";
+import { IconButton } from "react-native-paper";
+import { router } from "expo-router";
+import axios from "axios";
 
-const logo = require("@/assets/images/icon_laundrybuddy.png");
 const bg = require("@/assets/images/water.png");
 
+// TODO: Change "NOW" label to reflect current time's crowd by default
+// TODO: When a bar is selected, replace NOW with new time and its crowd analysis
+
 export default function Status() {
-  const key = "6274";
-  //TODO: Implement backend retrieval of key
-  const [token, setToken] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [cfmPassword, setCfmPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const toggleShowPassword = () => {
-    setShowPassword(!showPassword);
+  const footfallData = [
+    { value: 11, label: "12am" },
+    { value: 5, label: "1am" },
+    { value: 2, label: "2am" },
+    { value: 0, label: "3am" },
+    { value: 0, label: "4am" },
+    { value: 0, label: "5am" },
+    { value: 1, label: "6am" },
+    { value: 2, label: "7am" },
+    { value: 3, label: "8am" },
+    { value: 3, label: "9am" },
+    { value: 5, label: "10am" },
+    { value: 7, label: "11am" },
+    { value: 10, label: "12pm" },
+    { value: 12, label: "1pm" },
+    { value: 10, label: "2pm" },
+    { value: 10, label: "3pm" },
+    { value: 12, label: "4pm" },
+    { value: 13, label: "5pm" },
+    { value: 14, label: "6pm" },
+    { value: 16, label: "7pm" },
+    { value: 19, label: "8pm" },
+    { value: 20, label: "9pm" },
+    { value: 19, label: "10pm" },
+    { value: 15, label: "11pm" },
+  ];
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post(
+        `${process.env.EXPO_PUBLIC_API_URL}/api/auth/logout`
+      );
+      Alert.alert("Log out", response.data.msg);
+      router.back();
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status < 500
+      ) {
+        Alert.alert(
+          "Failed to set machine state",
+          `${error.response.data.msg}`
+        );
+      } else {
+        Alert.alert(
+          "Failed to set machine state",
+          `Please contact the developers with the following information\n\n${error}`
+        );
+        console.error(error);
+      }
+    }
   };
 
   return (
@@ -38,7 +80,37 @@ export default function Status() {
         resizeMode="cover"
         style={styles.imagebackground}
       >
-        <Text style={styles.title}>Summary</Text>
+        <View style={styles.optionsView}>
+          <IconButton
+            icon="account-circle"
+            size={50}
+            onPress={() => handleLogout()}
+          />
+          <IconButton
+            icon="cog"
+            size={50}
+            onPress={() => console.log("Settings pressed")}
+          />
+        </View>
+        <View style={styles.titleView}>
+          <Text style={styles.titleText}>Crowd Statistics</Text>
+          <View style={styles.crowdNowContainer}>
+            <Text style={styles.nowText}>Now</Text>
+            <Text style={styles.footfallText}> A little busy </Text>
+          </View>
+        </View>
+        <ScrollView contentContainerStyle={styles.graph}>
+          <View style={styles.chartContainer}>
+            <BarChart
+              data={footfallData}
+              horizontal
+              hideRules
+              spacing={10}
+              labelsDistanceFromXaxis={10}
+              xAxisThickness={0}
+            />
+          </View>
+        </ScrollView>
       </ImageBackground>
     </SafeAreaView>
   );
@@ -46,15 +118,15 @@ export default function Status() {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
-    flex: 1,
-    borderBlockColor: "white",
-
+    alignItems: "flex-start",
   },
-  image: {
-    height: 240,
-    width: 240,
-    justifyContent: "center",
+  crowdNowContainer: {
+    alignSelf: "flex-start",
+    borderColor: "white",
+    borderRadius: 10,
+    borderWidth: 2,
+    flexDirection: "row",
+    padding: 3,
   },
   imagebackground: {
     height: "100%",
@@ -62,95 +134,43 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  title: {
-    fontFamily: "Roboto",
-    fontSize: 40,
-    fontWeight: "bold",
-    textAlign: "center",
-    color: "darkblue",
-  },
-  subtitle: {
-    fontFamily: "Roboto",
-    fontSize: 17,
-    textAlign: "center",
-    paddingBottom: 20,
-    color: "black",
-    width: "60%",
-  },
-  tokenView: {
-    width: "100%",
-    paddingHorizontal: 50,
-    justifyContent: "center",
+  titleView: {
+    width: "70%",
     alignItems: "center",
-    flexDirection: "row",
+  },
+  graph: {
+    width: "90%",
+    height: 500,
+    alignContent: "center",
+  },
+  chartContainer: {
+    width: "90%",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  titleText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "darkblue",
     paddingBottom: 10,
   },
-  tokenText: {
-    fontSize: 17,
-    justifyContent: "center",
-    alignItems: "center",
+  optionsView: {
+    paddingTop: 50,
+    width: "100%",
     flexDirection: "row",
-    paddingRight: 10,
-    fontWeight: "bold",
+    justifyContent: "space-between",
   },
-  keyText: {
-    fontFamily: "Roboto",
-    fontSize: 17,
-    textAlign: "center",
-    color: "black",
-    width: 70,
-    backgroundColor: "lightblue",
-    borderRadius: 20,
+  nowText: {
+    fontSize: 16,
     padding: 5,
-  },
-  inputView: {
-    gap: 15,
-    width: "100%",
-    paddingHorizontal: 40,
-    marginBottom: 5,
-  },
-  input: {
-    height: 50,
-    paddingHorizontal: 20,
-    borderColor: "lightblue",
-    borderWidth: 2,
-    borderRadius: 20,
-  },
-  passwordInput: {
-    flex: 1,
-    height: 50,
-    paddingHorizontal: 20,
-    borderColor: "lightblue",
-    borderWidth: 2,
-    borderRadius: 20,
-  },
-  passwordView: {
-    width: "100%",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  icon: {
-    position: "absolute",
-    right: 20,
-  },
-  button: {
     backgroundColor: "blue",
-    height: 45,
-    borderColor: "gray",
-    borderWidth: 1,
     borderRadius: 5,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonText: {
     color: "white",
-    fontSize: 18,
-    fontWeight: "bold",
   },
-  buttonView: {
-    width: "100%",
-    paddingHorizontal: 50,
-    paddingTop: 10,
+  footfallText: {
+    fontSize: 16,
+    padding: 5,
+    borderRadius: 5,
+    color: "black",
   },
 });
